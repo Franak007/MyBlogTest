b986eeb8eff35d2ec99b544a4f486a44af7a6b7c
Franak007 <franck.pertosa@gmail.com>
2023-07-07 15:05:45 +0200

    sécurisation de l'enregistrement et création de 5 utilisateurs

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,9 +5,12 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -17,7 +20,39 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+            ->add('firstName', TextType::class, [
+                'constraints' => [
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Your First Name should be at least 3 characters',
+                    ]),
+                ],
+            ])
+            ->add('lastName', TextType::class, [
+                'constraints' => [
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Your Last Name should be at least 2 characters',
+                    ]),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new Email([
+                        'message' => 'Your Email is not valid',
+                    ]),
+                ],
+            ])
+            ->add('username', TextType::class, [
+                'constraints' => [
+                    new Length([
+                        'min' => 6,
+                        'max' => 16,
+                        'minMessage' => 'Your Username should be at least 6 characters',
+                        'maxMessage' => 'Your Username should be at max 16 characters',
+                    ]),
+                ],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
